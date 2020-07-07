a7f996ffd770b63fe9e26855ab88935b32efff67
shivaburade <shvburade@gmail.com>
2020-07-07 22:09:30 +0530

    mask notification

--- src/app/bluetooth.service.ts
+++ src/app/bluetooth.service.ts
@@ -10,6 +10,7 @@ export class BluetoothService {
 
   devices = [];
   errMsg : string;
+  db: SQLiteObject;
   constructor(private ble: BLE, private localNotifications: LocalNotifications, private sqlite:SQLite) { 
     
   }
@@ -23,8 +24,8 @@ export class BluetoothService {
     this.sqlite.create({
       name: 'data.db',
       location: 'default'}).then((db: SQLiteObject) => {
-        
-        db.executeSql("create table IF NOT EXISTS travel_tracing(mac_address varchar(100), latitude double, longitude double, date varchar(100))").then(
+        this.db = db;
+        db.executeSql("create table IF NOT EXISTS travel_tracing(mac_address varchar(100), latitude varchar(100), longitude varchar(100), date varchar(100))").then(
           () => {
             console.log('table created');
           }
@@ -45,41 +46,31 @@ export class BluetoothService {
     });
   }
 
+  insert_travel_tracing(query_data){
 
-  insert_profile(query){
-    this.sqlite.create({
-      name: 'data.db',
-      location: 'default'}).then((db: SQLiteObject) =>{
-        db.executeSql(query).then(()=>{
-          console.log('added');
-        });
+    var query = "insert into travel_tracing VALUES(?,?,?,?)";
+    this.db.executeSql(query,query_data ).then((data) => {
+      console.log('added');
+      console.log(data);
+    }).catch((data)=>{
+      console.log('not added');
+      console.log(data);
     });
   }
 
-  insert_containment_zone(query){
-    this.sqlite.create({
-      name: 'data.db',
-      location: 'default'}).then((db: SQLiteObject) =>{
-        db.executeSql(query).then(()=>{
-          console.log('added');
-        });
-    });
-  }
-
-  insert_travel_tracing(query){
-    this.sqlite.create({
-      name: 'data.db',
-      location: 'default'}).then((db: SQLiteObject) =>{
-        db.executeSql(query).then(()=>{
-          console.log('added');
-        });
+  get_travel_tracing(){
+   return this.db.executeSql('select * from travel_tracing', []).then((data) => {
+     var rows = [];
+     for (var i=0; i< data.rows.length; i++){
+      rows.push({ name: data.rows.item(i).mac_address});
+     }
+      return rows;
     });
   }
 
   social_distance_notification() {
-
+    this.Mask_notification();
     this.localNotifications.schedule({
-
       id: 1,
       title: 'Social distance alert',
       text: 'Please maintain social distance',
@@ -87,14 +78,12 @@ export class BluetoothService {
       vibrate: true,
       lockscreen: true,
       foreground:true
-
     });
   }
 
   contact_tracting_notification() {
 
     this.localNotifications.schedule({
-
       id: 2,
       title: 'Contact alert',
       text: 'You recently came in cotact with covid positive patient, take care and maintain hygiene',
@@ -102,7 +91,19 @@ export class BluetoothService {
       vibrate: true,
       lockscreen: true,
       foreground:true
+    });
+  }
 
+  Mask_notification() {
+
+    this.localNotifications.schedule({
+      id: 3,
+      title: 'Mask alert',
+      text: 'Please wear a mask!',
+      data: { secret: 'secret' },
+      vibrate: true,
+      lockscreen: true,
+      foreground:true
     });
   }
 
@@ -115,8 +116,6 @@ export class BluetoothService {
           
          var dev = this.devices.findIndex(d => d.id == device.id);
          
-         
-
          if(dev == -1 || this.devices.length == 0)
           {
             this.devices.push(device);
@@ -124,8 +123,8 @@ export class BluetoothService {
             if(device.rssi > -80){
               this.social_distance_notification();
               var date = new Date().getTime().toString()
-              var query = `insert into travel_tracing values(#{device.id}, 21.1562136, 72.77748539, #{date})`
-              this.insert_travel_tracing(query);
+              var query_data = [`${device.id}`, '21.1562136', '72.77748539', `${date}`];
+              this.insert_travel_tracing(query_data);
              }
           }else{
             this.devices[dev] = device;
